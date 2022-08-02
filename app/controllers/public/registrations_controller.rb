7af8c18e1eaa49e85880accd15c9ef817757468f
mimura15 <5101south@gmail.com>
2022-08-02 02:24:28 +0000

    [Add] guest sign_in

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,13 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  before_action :ensure_normal_user, only: :destroy
+
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admin
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'public/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+  end
 
   root to: 'public/homes#top'
 end
