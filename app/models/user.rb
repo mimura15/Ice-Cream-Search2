7365cdbdca1fa8128424dc8f37dfb346d6ebc056
mimura15 <5101south@gmail.com>
2022-08-02 07:05:49 +0000

    [Fix] ログイン関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "guest"
+    end
   end
 
 end
