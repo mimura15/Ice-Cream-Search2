b009e9b68f6d94fdfd6f9d2c42f04e552bce19c1
mimura15 <5101south@gmail.com>
2022-08-05 14:32:39 +0000

    [Add] admin user機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,15 +4,26 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user.id)
+    else
+      render :edit
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :message, :image, :is_active)
   end
 
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -31,6 +31,6 @@ class Public::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :message, :image,)
+    params.require(:user).permit(:name, :email, :message, :image)
   end
 end
