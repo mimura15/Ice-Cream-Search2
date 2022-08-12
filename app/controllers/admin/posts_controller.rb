099821276f3dab3628249f02d65d3c336d041719
mimura15 <5101south@gmail.com>
2022-08-12 06:19:03 +0000

    [Add] public/post new

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,18 @@
 class Admin::PostsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,30 @@
 class Public::PostsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+    @shops = Shop.all
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
