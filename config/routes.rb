5624b8d9381427d815062791a5139745a31a4f7d
mimura15 <5101south@gmail.com>
2022-08-14 11:52:11 +0000

    [Fix] devise sessionエラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin
   devise_for :users, controllers: {
-    registrations: 'public/registrations'
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
