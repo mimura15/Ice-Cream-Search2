c25f8059f48bd18604bce9aee830600aef71b1ac
mimura15 <5101south@gmail.com>
2022-08-14 02:13:32 +0000

    [Fix] public/post show

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,6 +6,7 @@ class Public::PostsController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = Post.find(params[:id])
+    @shop = Shop.find(params[:id])
   end
 
   def new
