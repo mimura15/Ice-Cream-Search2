769c046153a29be25d2b955ca470b99c5b3135a6
mimura15 <5101south@gmail.com>
2022-09-07 12:21:54 +0000

    [Add] タグ機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :shop
   has_many_attached :images
+  has_many :tag_maps, dependent: :destroy
+  has_many :tags, through: :tag_maps
 
   validates :title, presence: true
   validates :content, presence: true
